2ca65f5882ff2cdc656ea307d125f3b33d34e08f
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2020-12-13 17:02:31 -0300

    atualização Cururu2

--- Projeto 02 Java/Cururu2.java
+++ Projeto 02 Java/Cururu2.java
@@ -16,54 +16,71 @@ public class Cururu2{
     void comer(){
         if(bucho < maxBucho){
             bucho +=1;
-            System.out.println("Slp!! Glump!! Aaah!! Delicius!!");
+            System.out.println("\nSlp!! Glump!! Aaah!! Delicius!!\n");
         }else{
-            System.out.println("Nan manchuuu!! Tô espocando!!! 0.o");
+            System.out.println("\nNan manchuuu!! Tô espocando!!! 0.o\n");
           }
     }
 
     void pular(){
-        if(bucho > 0){
+        if(agua > 0 && bucho > 0){
             bucho -= 1;
             agua  -= 1;
-            System.out.println("Lá vou eu!! Jump!!");
-        }else{
-            System.out.println("Oh Fome!! Vou não!!");
+            System.out.println("\nLá vou eu!! Jump!!\n");
+        }else if(agua <= 0){
+            System.out.println("\nOh Sede!! Vou nada!!\n");
+        }else if(bucho <= 0){
+            System.out.println("\nOh Fome!! Vou não!!\n");
         }
     }
 
     void hidratar(){
         if(agua < maxAgua){
             agua += 1;
-            System.out.println("Humm!! Agua!!");
+            System.out.println("\nHumm!! Agua!!\n");
         }else{
-            System.out.println("Dxaqueto!! Já tô molhadinho!!");
+            System.out.println("\nDxaqueto!! Já tô molhadinho!!\n");
         }
     }
 
     void pegarSol(){
-        if(agua > 0){
+        if(agua > 0 && bucho > 0){
             agua -=1;
-            System.out.println("Impressão minha ou estou ficando com a pele ressecada??");
-        }else{
-            System.out.println("C tá doiiidooo!! Vou morreeee!! Carlor dos seissentos!! Quero água!!");
+            bucho -=1; 
+            System.out.println("\nSolzinho gostoso!! Impressão minha ou estou ficando com a pele ressecada??\n");
+        }else if(agua <= 0){
+            System.out.println("\nC tá doiiidooo!! Carlor dos seissentos!! Quero água!!\n");
+        }else if(bucho <= 0){
+            System.out.println("\nOh Fome!! Vou não!!\n");
         }
     }
 
+    void menu(){
+        System.out.println("\nOlá meu nome é cururu João!\n\nPara saber como podemos brincar digite: ajuda\n\n"); 
+    }
+
+    void ajuda(){
+        System.out.println("\nPara mostrar o estado do João digite: show.\nPara hidradar o João digite: beber.\nPara alimentar João digite: comer.\nPara João passear digite: pular.\nPara João tomar um sol digite: solzinho.\nPara sair do aplicação digite: end.\n\nDIVIRTA-SE!!\n\n");
+    }
+
     public String toString(){
-    return "Bucho: " + bucho + "/" + maxBucho + " Nível de água: " + agua + "/" + maxAgua;
+    return "\nBucho: " + bucho + "/" + maxBucho + " Nível de água: " + agua + "/" + maxAgua + "\n";
     }
     
     public static void main(String[] args) {
-        Cururu2 joao = new Cururu2(0, 10, 0, 10);
+        Cururu2 joao = new Cururu2(0, 5, 0, 5);
         
         Scanner scanner = new Scanner(System.in);
+
+        joao.menu();
         
         while(true){
             String line = scanner.nextLine();
             if(line.equals("end")){
-                System.out.println("¡Adios amigo!");
+                System.out.println("\n¡Adios amigo!\n");
                 break;
+            }else if(line.equals("ajuda")){
+                joao.ajuda();
             }else if(line.equals("show")){
                 System.out.println(joao);
             }else if(line.equals("comer")){
@@ -75,7 +92,7 @@ public class Cururu2{
             }else if(line.equals("solzinho") ){
                 joao.pegarSol();
             }else{
-                System.out.println("FAIL!!! EOQ MAN?!");
+                System.out.println("\nFAIL!!! EOQ MAN?!\n");
             }
         }
         scanner.close();             
